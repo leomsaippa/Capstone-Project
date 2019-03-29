0c70ed1888fca40827f9a35d3453e94d0b18a658
leomsaippa <leonardo_saippa@id.uff.br>
2019-03-28 23:30:47 -0300

    Adds LiveData and ViewModel

--- app/src/main/java/com/travelguide/ui/fragments/itineraryList/ItineraryListFragment.java
+++ app/src/main/java/com/travelguide/ui/fragments/itineraryList/ItineraryListFragment.java
@@ -1,14 +1,22 @@
 package com.travelguide.ui.fragments.itineraryList;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.travelguide.R;
+import com.travelguide.data.db.ItineraryDbHelper;
+import com.travelguide.data.network.model.Itinerary;
+import com.travelguide.ui.ItineraryViewlModel;
 import com.travelguide.ui.base.BaseFragment;
+
 
 import javax.inject.Inject;
 
@@ -16,6 +24,12 @@ import butterknife.ButterKnife;
 
 public class ItineraryListFragment extends BaseFragment implements ItineraryListMvpView {
 
+
+    public static final String TAG = ItineraryListFragment.class.getSimpleName();
+
+    private ItineraryViewlModel itinerariesViewModel;
+
+    ItineraryDbHelper mDb;
 
     @Inject
     ItineraryListMvpPresenter<ItineraryListMvpView> mPresenter;
@@ -40,8 +54,43 @@ public class ItineraryListFragment extends BaseFragment implements ItineraryList
 
         mPresenter.onAttach(this);
 
+        setupViewModel();
+
+        mDb = ItineraryDbHelper.getInstance(getContext());
+
+        loadItineraries();
+
         return view;
 
     }
 
+    private void loadItineraries() {
+        itinerariesViewModel.getItineraries().observe(this,
+                itineraries -> Log.d(TAG,"Load itineraries " + itineraries.get(0).getName()));
+    }
+
+
+    private void setupViewModel() {
+
+        itinerariesViewModel = ViewModelProviders.of(this)
+                .get(ItineraryViewlModel.class);
+    }
+
+    private void removeObservers() {
+        if ( itinerariesViewModel != null && itinerariesViewModel.getItineraries().hasObservers()){
+            itinerariesViewModel.getItineraries().removeObservers(this);
+        }
+    }
+
+    private void loadItinerary(int id) {
+        final LiveData<Itinerary> itinerary = mDb.itineraryDao().getItinerary(id);
+        itinerary.observe(this, new Observer<Itinerary>() {
+            @Override
+            public void onChanged(@Nullable Itinerary it) {
+                itinerary.removeObserver(this);
+                Log.d(TAG, "CheckFavorite " + it.getName());
+
+            }
+        });
+    }
 }
