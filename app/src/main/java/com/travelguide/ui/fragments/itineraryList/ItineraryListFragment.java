2c7f001a712b9517dc636a07c247819d089edf79
leomsaippa <leonardo_saippa@id.uff.br>
2019-04-20 21:29:04 -0300

    Removes unused code

--- app/src/main/java/com/travelguide/ui/fragments/itineraryList/ItineraryListFragment.java
+++ app/src/main/java/com/travelguide/ui/fragments/itineraryList/ItineraryListFragment.java
@@ -12,9 +12,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.travelguide.R;
 import com.travelguide.data.db.ItineraryDbHelper;
